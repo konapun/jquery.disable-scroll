129008a8a00ddb85228bfa26965d5a35dbff2cec
konapun <bremen.braun@gmail.com>
2013-09-25 14:08:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Disable Scroll
+A jQuery plugin to contextually disable/reenable page scrolling when an element is hovered over. This
+plugin is useful when you have an element, such as an iframe, which wants to intercept events like
+the scrollwheel without scrolling the page it's embedded in.
+
+## Usage
+```js
+$('#target').disableScroll(); // that's all there is!
+```
---
+++ jquery.disable-scroll.js
@@ -0,0 +1,28 @@
+/*
+ * A jQuery plugin that disables page scrolling when the target element is hovered over, 
+ * and restores scrolling when unhovered
+ 
+ * author: Bremen Braun
+ */
+;(function($) {
+	$.fn.disableScroll = function(opts) {
+		var $this = $(this),
+		    html = $('html');
+		
+		$this.mouseover(function() { // disable scrolling
+			var scrollPosition = [
+				$this.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+				$this.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+			];
+			
+			html.data('scroll-position', scrollPosition);
+			html.data('previous-overflow', html.css('overflow'));
+			html.css('overflow', 'hidden');
+			window.scrollTo(scrollPosition[0], scrollPosition[1]);
+		}).mouseout(function() { // restore scrolling
+			var scrollPosition = html.data('scroll-position');
+			html.css('overflow', html.data('previous-overflow'));
+			window.scrollTo(scrollPosition[0], scrollPosition[1])
+		});
+    }
+}(jQuery));
